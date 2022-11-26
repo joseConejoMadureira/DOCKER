3d34c593c3eb57a7c675e5656b31a54eafedb56a
Your <dev.estudos15@outlook.com>
2022-11-25 21:18:36 -0300

    clean

--- phpVue/backend/index.php
+++ phpVue/backend/index.php
@@ -2,33 +2,14 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-use src\Service\ProductService;
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 86400");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Credentials: true');
 
 $dotenv = Dotenv\Dotenv::createMutable(__DIR__);
 $dotenv->load();
 
-$uri = explode("/", $_SERVER['REQUEST_URI']);
-$method = $_SERVER['REQUEST_METHOD'];
-$service = $uri[2];
-
-$data = json_decode(file_get_contents("php://input"));
-$data = json_encode($data);
-LogsW::write("input: ".$data);
-switch ($service) {
-        //PRODUCT
-    case 'product':
-        LogsW::write('CASE: product');
-        $product  = new  ProductService($uri, $method, $data);
-        echo  json_encode($product->route());
-        break;
-        // Y
-    case 'teste':
-        echo   'server ok';
-        break;
-   
-    default:
-        LogsW::write('Service Not Found');
-        header("HTTP/1.1 404 Not Found");
-        echo json_encode("Service Not Found");
-        die();
-}
+echo 'ok';
