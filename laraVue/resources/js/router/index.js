fc5f32de4fcf172b071bc3a1b6dd72a3d71112b9
joseConejoMadureira <dev.estudos15@outlook.com>
2022-11-29 19:11:38 -0300

    vuejs route ok

--- laraVue/resources/js/router/index.js
+++ laraVue/resources/js/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
@@ -18,7 +18,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
