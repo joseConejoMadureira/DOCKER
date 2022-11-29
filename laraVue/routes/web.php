c398390d476189f73682a23f28e6d279e59043ea
joseConejoMadureira <dev.estudos15@outlook.com>
2022-11-29 20:59:24 -0300

    rest

--- laraVue/routes/web.php
+++ laraVue/routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 Route::get('{any}', function () {
     return view('index');
 })->where("any", ".*");
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
