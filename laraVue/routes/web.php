48b5314885ca498e5dd1d6a91c47812d4eb82cf1
joseConejoMadureira <dev.estudos15@outlook.com>
2022-11-29 16:18:09 -0300

    routes

--- laraVue/routes/web.php
+++ laraVue/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
+Route::get('{any}', function () {
     return view('welcome');
-    })->where("any",".*");
+})->where("any", ".*");
