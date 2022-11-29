18ccdae7d2ebd4ad6208dbd0678571a0b2036ce5
joseConejoMadureira <dev.estudos15@outlook.com>
2022-11-29 19:35:30 -0300

    laravel auth

--- laraVue/routes/web.php
+++ laraVue/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('{any}', function () {
     return view('index');
 })->where("any", ".*");
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
