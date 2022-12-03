6ae8f989b1156d9adcb0316d75139b98a2508c3a
joseConejoMadureira <jose.games15@outlook.com>
2022-12-03 15:25:19 -0300

    espacos

--- laraVue/resources/views/index.blade.php
+++ laraVue/resources/views/index.blade.php
@@ -3,9 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel 9 with vue 3 vite</title>
-
     </head>
     <body>
         <div id="app"></div>
