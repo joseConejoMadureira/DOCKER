617bcb7200375974180b9724543256c7bd56803a
joseConejoMadureira <jose.games15@outlook.com>
2022-12-04 18:37:50 -0300

    cache

--- laraVue/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('test','App\Http\Controllers\TestController');
--- laraVue/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::get('{any}', function () {
-    return view('index');
-})->where("any", ".*");
