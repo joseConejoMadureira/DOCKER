5d7d2b0d853465e1e956e0944e180190212e01ff
joseConejoMadureira <jose.games15@outlook.com>
2022-12-05 15:05:07 -0300

    dbinit

--- laraVue/mysql/schema.sql
+++ laraVue/mysql/schema.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE laravel;
+
+USE laravel;
+
+CREATE TABLE `laravel`.`log` (
+  	`ID` bigint NOT NULL AUTO_INCREMENT,
+  	`DESCRIPTION` varchar(100) DEFAULT NULL,
+  	PRIMARY KEY (`ID`)
+	);
